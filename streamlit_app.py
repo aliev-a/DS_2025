fac87809551f7a38ee8468207a27baad91ff4fb4
A.Abubakr <57147605+aliev-a@users.noreply.github.com>
2025-11-21 20:06:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ with st.expander("DATA"):
   st.write('**OUR DATA**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+with st.expander("Visualization"):
+  st.scatter_chart(data=df, x='bill_lenght_mm', y='bill_depth_mm', color='species')
