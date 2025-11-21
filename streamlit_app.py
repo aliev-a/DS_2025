b1c43a0e207a6c91b027d4dacb3c27d10b183a69
A.Abubakr <57147605+aliev-a@users.noreply.github.com>
2025-11-21 19:59:22 +0500

    Load and display penguins dataset in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ st.write('Lets do it!')
 
 with st.expander("DATA"):
   st.write('**OUR DATA**')
-  
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
