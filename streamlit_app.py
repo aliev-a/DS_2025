8fa269e57c125ec499c99552abc241ffead1c35f
A.Abubakr <57147605+aliev-a@users.noreply.github.com>
2025-11-21 20:10:30 +0500

    Add sidebar for input features in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,6 @@ with st.expander("DATA"):
 
 with st.expander("Visualization"):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+with st.sidebar:
+  st.header('Input features')
