b810936df27eb110ac70e68b8d1ff10ba20db8c9
A.Abubakr <57147605+aliev-a@users.noreply.github.com>
2025-11-24 18:51:41 +0500

    Implement user input features in Streamlit app
    Add input features for user interaction and data preparation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ with st.expander("DATA"):
 with st.expander("Visualization"):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
+X_raw = df.drop('species', axis=1)
+y_raw = df.species
+
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('**Island**', ('Torgersen', 'Dream', 'Biscoe'))
@@ -21,3 +24,12 @@ with st.sidebar:
   flipper_length_mm = st.slider("**flipper_length_mm**", 32.1, 59.6, 55.1)
   body_mass_g = st.slider("**body_mass_g**", 2700, 6300, 4207)
   sex = st.selectbox('**Gender**', ('male', 'female'))
+
+data = {'island':island,
+        'bill_length_mm':bill_length_mm,
+        'bill_depth_mm':bill_depth_mm,
+        'flipper_length_mm':flipper_length_mm,
+        'body_mass_g':body_mass_g,
+        'sex':sex}
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X_raw], axis=0)
